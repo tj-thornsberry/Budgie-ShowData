d7a77a2afcb0de5b26509da1b074f3f511e272f1
tj-thornsberry <tj_thornsberry@hotmail.com>
2020-05-07 13:08:50 -0600

    Update datatools.py

--- datatools.py
+++ datatools.py
@@ -12,8 +12,22 @@ from gi.repository import Pango, Gdk
 
 
 """
-Budgie ShowData
-Author: TJ Thornsberry
+    Budgie ShowData - Shows Output of vnstat on the desktop
+    Authors: TJ Thornsberry, Jacob Vlijm
+    Copyright (C) 2020  Ubuntu Budgie Developers
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 # paths
